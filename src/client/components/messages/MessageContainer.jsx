690f38328f2647568dd533c126fdfa66bcf8483d
William Okou <gokou097@gmail.com>
2024-03-14 02:38:47 +0000

    zustand added for chat state

--- src/client/components/messages/MessageContainer.jsx
+++ src/client/components/messages/MessageContainer.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import MessageInput from './MessageInput';
 import Messages from './Messages';
 import {TiMessages} from 'react-icons/ti';
+import useConversation from '../../zustand/useConversation';
+import {useEffect} from 'react';
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const {selectedConversation, setSelectedConversation} = useConversation();
+
+  useEffect(() => {
+    // This is a cleanup function that runs when the component unmounts
+    return () => {
+      setSelectedConversation(null);
+    };
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           <div className="Header-container bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:</span>{' '}
-            <span className="text-gray-900 font-bold">Goku</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.username}
+            </span>
           </div>
 
           <Messages />
--- src/client/components/sidebar/Conversation.jsx
+++ src/client/components/sidebar/Conversation.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {FaRegCircleUser} from 'react-icons/fa6';
+import useConversation from '../../zustand/useConversation';
 
-const Conversation = () => {
+const Conversation = ({conversation, lastIdx}) => {
+  const {selectedConversation, setSelectedConversation} = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
+
   return (
     <>
-      <div className="Conversation container flex gap-2 items-center hover:bg-blue-500 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`Conversation-container flex gap-2 items-center hover:bg-blue-500 rounded p-2 py-1 cursor-pointer
+    ${isSelected && 'bg-blue-500'}`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="Avatar-container avatar">
           <div className="w-12 rounded-full">
             <FaRegCircleUser className="w-full h-full" />
@@ -13,12 +22,12 @@ const Conversation = () => {
 
         <div className="Username-container flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">Goku</p>
+            <p className="font-bold text-gray-200">{conversation.username}</p>
           </div>
         </div>
       </div>
 
-      <div className="divider my-0 py-0 h-1" />
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
